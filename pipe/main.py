2c7df95fff6a86bcd11f8ae5bdcae1084a420781
Scott Milner <scottdmilner@gmail.com>
2024-01-04 15:52:27 -0700

    get logging working (copy 2024 config)

--- pipe/main.py
+++ pipe/main.py
@@ -9,13 +9,27 @@ arguments, then run launch(). If pipe is not specified, use the value in
 the BYU_FILM_PIPE environment variable.
 """
 
+import logging
 import os
+
 from argparse import ArgumentParser
 
 from lib.util import find_implementation
 from software.baseclass import DCC
 
 pipe_true_root = os.path.realpath(os.path.dirname(__file__))
+
+
+# Configure logging
+log = logging.getLogger(__name__)
+
+
+def getLevelNamesMapping():
+    """Implement the same-named method from the logging module.
+
+    TODO: REPLACE ONCE OUR PYTHON IS >= 3.11
+    """
+    return logging._nameToLevel.keys()
 
 
 def launch(software_name: str) -> None:
@@ -29,15 +43,23 @@ if __name__ == "__main__":
         "software",
         help="launch the specified software",
     )
-    # parser.add_argument(
-    #     '-l', '--log-level',
-    #     help="log at the specified level. Possible values are %(choices)s (default: %(default)s)",
-    #     choices=getLevelNamesMapping(),
-    #     default=logging.getLevelName(logging.root.level),
-    #     type=str.upper,
-    #     metavar='LEVEL',
-    # )
+    parser.add_argument(
+        "-l",
+        "--log-level",
+        help="log at the specified level. Possible values are %(choices)s (default: %(default)s)",
+        choices=getLevelNamesMapping(),
+        default=logging.getLevelName(logging.root.level),
+        type=str.upper,
+        metavar="LEVEL",
+    )
 
     args = parser.parse_args()
 
+    logging.basicConfig(
+        level=args.log_level,
+        format="%(asctime)s %(processName)s(%(process)s) %(threadName)s [%(name)s(%(lineno)s)] [%(levelname)s] %(message)s",
+    )
+
     launch(args.software)
+
+    log.info("Exiting")
