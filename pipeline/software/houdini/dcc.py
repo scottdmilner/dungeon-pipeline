1a2ec25c8b4f7b4a6df7d54a946c1f94c0550763
Scott Milner <scottdmilner@gmail.com>
2024-01-15 15:06:25 -0700

    Default houdini desktop

--- pipeline/software/houdini/dcc.py
+++ pipeline/software/houdini/dcc.py
@@ -19,34 +19,49 @@ class HoudiniDCC(DCC):
         pipe_path = this_path.parents[2]
 
         system = platform.system()
+        hfs = ""
+        if system == "Linux":
+            hfs = Path("/opt/hfs19.5.640").resolve()
+        elif system == "Windows":
+            hfs = Path(
+                "C:\\Program Files\\Side Effects Software\\Houdini 19.5.640"
+            ).resolve()
+        else:
+            raise NotImplementedError(
+                f"The operating system {system} is not a supported OS for this DCC software"
+            )
 
         env_vars = {
-            "HOUDINI_BACKUP_DIR": "./.backup",  # Backup directory
-            "HOUDINI_COREDUMP": 1,  # Dump the core on crash to help debugging
-            "HOUDINI_MAX_BACKUP_FILES": 20,  # Max backup files
-            "HOUDINI_NO_ENV_FILE_OVERRIDES": 1,  # Prevent user envs from overriding existing values
+            # Backup directory
+            "HOUDINI_BACKUP_DIR": "./.backup",
+            # Dump the core on crash to help debugging
+            "HOUDINI_COREDUMP": 1,
+            # Max backup files
+            "HOUDINI_MAX_BACKUP_FILES": 20,
+            # Prevent user envs from overriding existing values
+            "HOUDINI_NO_ENV_FILE_OVERRIDES": 1,
+            # Project-specific preference overrides
+            "HSITE": str(this_path.parent / "hsite"),
             # TODO: revert to project OCIO with R26
             # "OCIO": str(pipe_path / "lib/ocio/love-v01/config.ocio"),
-            "OCIO": str(Path("/opt/pixar" if system == "Linux" else "C:\\Program Files\\Pixar").resolve() / "RenderManProServer-25.2/lib/ocio/ACES-1.2/config.ocio"),
+            "OCIO": str(
+                Path(
+                    "/opt/pixar" if system == "Linux" else "C:\\Program Files\\Pixar"
+                ).resolve()
+                / "RenderManProServer-25.2/lib/ocio/ACES-1.2/config.ocio"
+            ),
             "PYTHONPATH": "",
             # "RMAN_COLOR_CONFIG_DIR": str(pipe_path / "lib/ocio/love-v01"),
         }
 
         launch_command = ""
-        if system == "Linux":
-            if is_python_shell:
-                launch_command = "/opt/hfs19.5.640/bin/hython"
-            else:
-                launch_command = "/opt/hfs19.5.640/bin/houdinifx"
-        elif system == "Windows":
-            if is_python_shell:
-                launch_command = "C:\\Program Files\\Side Effects Software\\Houdini 19.5.640\\bin\\hython.exe"
-            else:
-                launch_command = "C:\\Program Files\\Side Effects Software\\Houdini 19.5.640\\bin\\houdinifx.exe"
+        if is_python_shell:
+            launch_command = str(hfs / "bin/hython")
         else:
-            raise NotImplementedError(
-                f"The operating system {system} is not a supported OS for this DCC software"
-            )
+            launch_command = str(hfs / "bin/houdinifx")
+
+        if system == "Windows":
+            launch_command += ".exe"
 
         launch_args = [] if is_python_shell else ["-foreground"]
 
