ca15bb0db6822ae4a1721b21060dc9f1e176ae6d
Jack Gashler <jgashler44@gmail.com>
2024-03-30 17:13:31 -0600

    set workspace to /groups/dungeons on startup

--- pipeline/software/maya/userSetup/userSetup.py
+++ pipeline/software/maya/userSetup/userSetup.py
@@ -1,7 +1,7 @@
 """Initialize Maya environment on startup"""
 
 import maya.cmds as mc
-
+import pipe.util
 
 def main():
     if "mayaUsdPlugin" not in mc.pluginInfo(q=True, listPlugins=True):
@@ -10,3 +10,5 @@ def main():
 
 if not mc.about(batch=True):
     mc.evalDeferred(main)
+
+mc.workspace(str(pipe.util.get_production_path().parent), openWorkspace=True)
