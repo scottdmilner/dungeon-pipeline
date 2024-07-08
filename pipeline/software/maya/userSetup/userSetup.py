28ac7df6ee3eb124cddf837b886d0d54cfdf26fe
Scott Milner <scottdmilner@gmail.com>
2024-07-08 12:48:46 -0600

    add maya-timeline-marker plugin

--- pipeline/software/maya/userSetup/userSetup.py
+++ pipeline/software/maya/userSetup/userSetup.py
@@ -4,19 +4,24 @@ import maya.cmds as mc
 
 
 def main():
+    # Enable required blugins
     plugins = [
-        "AbcExport",
         "mayaUsdPlugin",
     ]
     pluginInfo = mc.pluginInfo(q=True, listPlugins=True)
-
     for plugin in plugins:
         if plugin not in pluginInfo:
             mc.loadPlugin(plugin)
 
     import pipe
 
+    # set workspace
     mc.workspace(str(pipe.util.get_production_path().parent), openWorkspace=True)
+
+    # enable timeline-marker plugin
+    from timeline_marker import install  # type: ignore[import-not-found]
+
+    install.execute()
 
 
 if not mc.about(batch=True):
