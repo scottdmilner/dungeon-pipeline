a770df88bef0cf19afff24e7b7ccc297d2a682ec
katie01bug05 <katie01bug05@gmail.com>
2024-07-13 10:21:11 -0600

    changing to correct file path. Locators Update

--- pipeline/pipe/m/ToolBox/Updating_Scripts.py
+++ pipeline/pipe/m/ToolBox/Updating_Scripts.py
@@ -15,7 +15,7 @@ def get_locator_position(locator_name):
 
 # Function to update the script with the new locator positions
 def update_script():
-    script_path = "pipe.m.toolbox.Robin_Face_Locators.py"
+    script_path = "pipe.m.ToolBox.Robin_Face_Locators.py"
     with open(script_path, "w") as file:
         file.write("# Building the Face\n# Locators\n\n")
         file.write('import maya.cmds as cmds")\n\n')
