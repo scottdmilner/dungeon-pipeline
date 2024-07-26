385c35ad6ad0fb0ad6033aaec65ca324380e583a
Scott Milner <scottdmilner@gmail.com>
2024-07-25 20:45:44 -0600

    move SP shelf out of the git repo

--- pipeline/software/substance_painter/plugins/startup/shelf.py
+++ pipeline/software/substance_painter/plugins/startup/shelf.py
@@ -2,14 +2,16 @@ from __future__ import annotations
 
 import substance_painter as sp
 
-from shared.util import get_pipe_path
+from shared.util import get_production_path
 
 _SHELF_NAME = "lnd-resources"
 
 
 def start_plugin():
     # create the LnD shelf
-    sp.resource.Shelves().add(_SHELF_NAME, str(get_pipe_path() / "lib/sp_assets"))
+    sp.resource.Shelves().add(
+        _SHELF_NAME, str(get_production_path() / "painter_assets")
+    )
 
 
 def close_plugin():
