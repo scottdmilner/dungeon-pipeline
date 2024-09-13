aeb87913089e4d89943ca31730a9979c3e105de3
Scott Milner <scottdmilner@gmail.com>
2024-09-13 14:19:30 -0600

    find all pickers in directory

--- pipeline/pipe/m/picker.py
+++ pipeline/pipe/m/picker.py
@@ -2,17 +2,13 @@ import dwpicker
 
 from shared.util import get_rigging_path
 
-picker_filenames = [
-    "Ray_Facial_Picker.json",
-    "Rayden_Picker.json",
-    "Robin_Picker.json",
-]
-
 
 def run():
     picker_folder_path = get_rigging_path() / "Pickers"
 
-    picker_filepaths = [str(picker_folder_path / pfn) for pfn in picker_filenames]
+    picker_filepaths = [
+        str(p) for p in picker_folder_path.iterdir() if p.suffix == ".json"
+    ]
     print("Picker filepaths", picker_filepaths)
 
     dwpicker.show(pickers=picker_filepaths)
