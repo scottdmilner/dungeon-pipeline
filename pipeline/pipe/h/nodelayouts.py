d8af862fbfff635e87389885d8d4cc9c282fbb6f
Isaac Criddle <87104088+isaac-criddle@users.noreply.github.com>
2024-09-27 17:06:48 -0600

    Dallin Clark
    Created an HDA called component cluster that allows for multiple assests to combing and create a new asset for easier layout

--- pipeline/pipe/h/nodelayouts.py
+++ pipeline/pipe/h/nodelayouts.py
@@ -9,7 +9,62 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from typing import Optional
 
-"""Scripts for building node setups. Called by lnd_nodelayouts.hdanc in Interactive > Shelf Tools"""
+
+"""Scripts for building node setups. Called by lnd_nodelayouts.hdanc in Interactive > Shelf Tools, also make sure to check a network context in the context tab"""
+def lnd_clustersetup(kwargs: dict, parent: Optional[hou.Node] = None) -> hou.Node:
+    out: hou.LopNode = loptoolutils.genericTool(kwargs, "componentoutput")
+    out.setColor(hou.Color((0.616, 0.871, 0.769)))
+
+    out_pos = out.position()
+
+    #This is the context within the out node exists, which should be the stage context
+    p = out.parent() 
+
+    #fetches the other nodes
+    ldv = p.createNode("sdm223::dev::LnD_Lookdev")
+    prim = p.createNode("primitive")
+    graft = p.createNode("graftstages")
+    env = p.createNode("fetch")
+    err = p.createNode("error")
+
+    #Establishes Connections
+    out.setInput(0, graft)
+    graft.setInput(0, err)
+    err.setInput(0,prim)
+    ldv.setInput(0, out)
+    out.setInput(1, env)
+
+    # Arrange nodes in "Y" shape
+    err_move = hou.Vector2(-1.22,2.3)
+    prim_move = hou.Vector2(-1.22, 3.5)
+    graft_move = hou.Vector2(0.0, 1.0)
+    ldv_move = hou.Vector2(0.0, -1.0)
+    env_move = hou.Vector2(1.5, 0.5)
+    prim.setPosition(prim_move + out_pos)
+    err.setPosition(err_move + out_pos)
+    graft.setPosition(graft_move + out_pos)
+    ldv.setPosition(ldv_move + out_pos)
+    env.setPosition(env_move + out_pos)
+
+    # Configure environment fetch
+    env.parm("loppath").set(f"../{ldv.name()}/OUT_ENV")
+
+    # Configure Component Output node
+    out.parm("mode").set(1)
+    graft.parm("destpath").set("/")
+    prim.parm("primpath").set("$OS")
+    out.parm("rootprim").set("`lopinputprim('.', 0)`")
+    err.parm("errormsg1").set("Please name your primitive node")
+    err.parm("severity1").set("error")
+
+    error_expression = "import re\nrgx = re.compile(\"primitive[0-9]+\")\nreturn any(rgx.match(node.name()) for node in hou.pwd().inputAncestors())"
+    err.parm("enable1").setExpression(error_expression, language=hou.exprLanguage.Python)
+
+    #Set the Component Output as Selected
+    out.setCurrent(True)
+    out.setSelected(True, clear_all_selected=True)
+
+    return out
 
 
 def lnd_componentgeometry(kwargs: dict, parent: Optional[hou.Node] = None) -> hou.Node:
