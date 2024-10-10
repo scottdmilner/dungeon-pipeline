477c6b437aaca01f7f1360645414114f5a31c2ba
Scott Milner <scottdmilner@gmail.com>
2024-10-10 11:57:49 -0600

    Fix pipe reload function in headless maya

--- pipeline/pipe/m/reload.py
+++ pipeline/pipe/m/reload.py
@@ -1,8 +1,14 @@
 def reload_pipe() -> None:
-    import mayaUsd.lib as mayaUsdLib  # type: ignore[import-not-found]
-    from pipe.m.usdchaser import ExportChaser
     from pipe.util import reload_pipe as _reload_pipe
 
     _reload_pipe()
-    mayaUsdLib.ExportChaser.Unregister(ExportChaser, ExportChaser.ID)
-    mayaUsdLib.ExportChaser.Register(ExportChaser, ExportChaser.ID)
+
+    # wrap this in a try block because it will fail in headless mode
+    try:
+        import mayaUsd.lib as mayaUsdLib  # type: ignore[import-not-found]
+        from pipe.m.usdchaser import ExportChaser
+
+        mayaUsdLib.ExportChaser.Unregister(ExportChaser, ExportChaser.ID)
+        mayaUsdLib.ExportChaser.Register(ExportChaser, ExportChaser.ID)
+    except Exception:
+        pass
