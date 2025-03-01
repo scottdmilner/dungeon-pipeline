098fe86f3959e8ba36164761e101167d23f3732f
spencerBlanchard <spenseriv@gmail.com>
2025-03-01 13:25:03 -0700

    added in button for the L&D write tool

--- pipeline/software/nuke/tools/NungeonTools/menu.py
+++ pipeline/software/nuke/tools/NungeonTools/menu.py
@@ -8,11 +8,33 @@ nuke.pluginAddPath("./nk_files")
 nuke.pluginAddPath("./toolsets")
 nuke.pluginAddPath("./scripts")
 
+# aspect ratio
+nuke.addFormat("1920 816 Love_and_Dungeons_aspect_ratio")
 
 
-# Nungeon buttons
+def make_ld_write_node():
+    import ld_write_node  # type: ignore[import-not-found]
+
+    ld_write_node.main()
+
+
+def import_render_layers():
+    import render_layer_selector  # type: ignore[import-not-found]
+
+    render_layer_selector.run()
+
+
+def choose_shot():
+    import open_shot  # type: ignore[import-not-found]
+
+    open_shot.run()
+
+
+################################### Nungeon buttons (Sidebar) ###################################
 toolbar = nuke.menu("Nodes")
 m = toolbar.addMenu("Nungeon", icon="nungeonIcon.png")
+
+# lens node
 m.addCommand("Lens", "nuke.createNode('Lens')", icon="nungeonIcon.png")
 print(
     f"nuke.nodePaste({str(get_pipe_path() / 'software/nuke/tools/NungeonTools/toolsets/shotTemplate.nk')})"
@@ -24,29 +46,10 @@ m.addCommand(
 )
 m.addCommand("FrameBurn", "nuke.createNode('FrameBurn')", icon="nungeonIcon.png")
 
-
-# aspect ratio
-nuke.addFormat("1920 816 Love_and_Dungeons_aspect_ratio")
-
-# Shelf Tools
-def import_render_layers():
-    import render_layer_selector  # type: ignore[import-not-found]
-
-    render_layer_selector.run()
+m.addCommand("L&D Write Node", "make_ld_write_node()", icon="nungeonIcon.png")
 
 
-def choose_shot():
-    import open_shot  # type: ignore[import-not-found]
-    open_shot.run()
-
-menu = nuke.menu('Nuke')
-menu.addCommand('Choose Shot', 'choose_shot()')
-menu.addCommand('Import Render Layers', 'import_render_layers()')
-
-
-
-
-print("Nungeon loaded successfully")
-print(
-    "Isaac is a robot. If you train him the same way you train an AI model you will get good results."
-)
+################################### Nungeon Shelf Tool Buttons ###################################
+menu = nuke.menu("Nuke")
+menu.addCommand("Choose Shot", "choose_shot()")
+menu.addCommand("Import Render Layers", "import_render_layers()")
