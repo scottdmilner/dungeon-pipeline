060615384b63cf42db23a5d08d45dca8a19cc795
spencerBlanchard <spenseriv@gmail.com>
2025-03-01 21:22:35 -0700

    changed what happens if the file is not saved

--- pipeline/software/nuke/tools/NungeonTools/scripts/set_frameRange_and_aspectRatio.py
+++ pipeline/software/nuke/tools/NungeonTools/scripts/set_frameRange_and_aspectRatio.py
@@ -15,31 +15,39 @@ def get_project_name():
         project_name = "Unsaved Project"
     return project_name
 
+
 def get_frame_range(project_name):
     conn = DB.Get(DB_Config)
-    
     shot_info = conn.get_shot_by_code(project_name)
     return [shot_info.cut_in, shot_info.cut_out]
 
 
 def set_frame_range(frame_in, frame_out):
-    nuke.root()['first_frame'].setValue(frame_in)
-    nuke.root()['last_frame'].setValue(frame_out)
-    nuke.root()['lock_range'].setValue(True)
-    
+    nuke.root()["first_frame"].setValue(frame_in)
+    nuke.root()["last_frame"].setValue(frame_out)
+    nuke.root()["lock_range"].setValue(True)
+
 
 def set_full_frame_size(root_node):
-    root_node['format'].setValue('Love_and_Dungeons_aspect_ratio')
+    root_node["format"].setValue("Love_and_Dungeons_aspect_ratio")
+
 
 def set_frame_rate(root_node):
-    root_node['fps'].setValue(24)
+    root_node["fps"].setValue(24)
+
 
 def run():
-    frame_in, frame_out = get_frame_range(get_project_name())
-    set_frame_range(frame_in, frame_out)
+    project_name = get_project_name()
     root_node = nuke.root()
     set_full_frame_size(root_node)
-    set_frame_rate(root_node)
+
+    # set frame range if the file is saved
+    if project_name == "Unsaved Project":
+        return
+    else:
+        frame_in, frame_out = get_frame_range(project_name)
+        set_frame_range(frame_in, frame_out)
+        set_frame_rate(root_node)
 
 
 run()
